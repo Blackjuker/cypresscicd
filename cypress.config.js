6a3e884d368695ea363ec6ae455ca6c2a096cea4
Blackjuker <ngabgnaarnaud2@gmail.com>
2025-03-05 12:41:14 +0100

    add artefacts

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,17 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter', // a ajouter
+  //reporter: 'cypress-mochawesome-reporter', // a ajouter
+  reporter: 'junit', 
   e2e: {
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);// a ajouter
       require('@cypress/grep/src/plugin')(config);
       return config;
+    },
+  reporterOptions: {
+      mochaFile: 'results/my-test-output.xml',
+      toConsole: true,
     },
   },
 });
