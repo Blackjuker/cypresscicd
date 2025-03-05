58be2bfd051e5059ef608130ba4146d8297a3c71
Blackjuker <ngabgnaarnaud2@gmail.com>
2025-03-05 13:50:13 +0100

    update config

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,15 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter', // a ajouter
-  reporter: 'junit',
+  reporter: 'junit', // Set to junit only
   reporterOptions: {
-    mochaFile: 'results/my-test-output-[hash].xml',
+    mochaFile: 'results/my-test-output-[hash].xml', // Specify the output path for the JUnit reports
+    toConsole: true,
   },
-  
   e2e: {
     setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on);// a ajouter
-      require('@cypress/grep/src/plugin')(config);
+      // Remove the mochawesome reporter setup since you're not using it
+      require('@cypress/grep/src/plugin')(config); // Keep grep plugin if you're using it
       return config;
     },
   },
